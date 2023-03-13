58c1d47b87701c8e6a614082d9d66ad9358c8224
Santhosh <santhosh@macraeconsultingcorp.com>
2023-03-13 14:34:10 -0500

    update

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -7,22 +7,22 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./App";
+import App from './App';
 
 const mount = (el) => {
-    ReactDOM.render(
-        <App />, 
-        el
-    )
+  ReactDOM.render(<App />, el);
+};
+
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
+
+  if (devRoot) {
+    mount(devRoot);
+  }
 }
 
-//Development
-if(process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
-    if(devRoot) {
-        mount(devRoot);
-    }
-}
-
-//For production
+// We are running through container
+// and we should export the mount function
 export { mount };
